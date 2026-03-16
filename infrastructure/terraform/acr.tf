00e3a4f828f77f0c5d9bf6d1aa799f6bb7623b2f
Sabri <91134638+S4BR1@users.noreply.github.com>
2026-03-16 15:20:15 +0100

    Add Azure Container Registry resource configuration

--- infrastructure/terraform/acr.tf
+++ infrastructure/terraform/acr.tf
@@ -1,1 +1,7 @@
-
+resource "azurerm_container_registry" "acr" {
+  name                = "aiobsacr2026"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = false
+}
