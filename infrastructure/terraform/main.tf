a9e9e6d840f0a843b1ce2922c91f0981d8a2f121
Sabri <91134638+S4BR1@users.noreply.github.com>
2026-03-16 15:19:36 +0100

    Update main.tf

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,1 +1,18 @@
+terraform {
+  required_version = ">= 1.10.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.80"
+    }
+  }
+}
 
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "ai-observability-rg"
+  location = "France Central"
+}
