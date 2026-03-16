397d6c7bc10a35b5e952256d8217316d7c2a59ec
Sabri <91134638+S4BR1@users.noreply.github.com>
2026-03-16 15:20:56 +0100

    Update repo_analyzer.py

--- platform/repo_analyzer.py
+++ platform/repo_analyzer.py
@@ -1,1 +1,14 @@
+import os
+import subprocess
+import glob
+from ai_stack_detector import detect_stack
 
+def clone_repo(url, path="repo"):
+    subprocess.run(["git", "clone", url, path], check=True)
+
+def analyze_repo(path="repo"):
+    files = [f for f in glob.glob(path + "/**/*", recursive=True)]
+    summary = "\n".join(files)
+
+    stack_info = detect_stack(summary)
+    return stack_info
